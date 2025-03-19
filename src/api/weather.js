1c3a968cbc8ba476b6442d00a985bf1e343b1821
Vidhey Bhogadi <vidhey.bhogadi2003@gmail.com>
2025-03-19 09:00:18 +0530

    First Commit

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a2613b1853c436afd1413d2e9bfc835a';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchWeather = async (city) => {
